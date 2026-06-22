0a7d6bd3787ace511ae6cc562b359fb36ad865b5
gargi-sharma18 <2005gargisharma18@gmail.com>
2026-06-22 23:59:24 +0530

    math and expoential

---
+++ Math/1922. Count Good Numbers/Solution.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int countGoodNumbers(long n) {
+
+        long evenPosition = (n + 1) / 2;
+        long oddPosition = n / 2;
+        long mod = 1000000007;
+
+        long even = calculate(5, evenPosition);
+        long odd = calculate(4, oddPosition);
+
+        return (int) ((even * odd) % mod);
+    }
+
+    public long calculate(long base, long power) {
+        long mod = 1000000007;
+
+        long ans = 1;
+        while (power > 0) {
+            if (power % 2 == 1) {
+                ans = (ans * base) % mod;
+            }
+            base = (base * base) % mod;
+            power = power / 2;
+        }
+        return ans;
+    }
+}
