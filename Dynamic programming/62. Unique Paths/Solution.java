12ec9de95a0c318e03ff6a3e44bac2c53604e8e8
gargi-sharma18 <2005gargisharma18@gmail.com>
2026-08-29 18:41:24 +0530

    most optimal approch

--- Dynamic programming/62. Unique Paths/Solution.java
+++ Dynamic programming/62. Unique Paths/Solution.java
@@ -1,24 +1,46 @@
 class Solution {
-    // optimal approch where space is O(2 * n) = O(n)
+    // most optimal approch where space is O(2 * n) = O(n)
     public int uniquePaths(int m, int n) {
         int[][] dp = new int[2][n];
-        for(int i = 0;i < n;i++){ // filling 0th row by 1
-            dp[0][i] = 1; 
+        for (int i = 0; i < n; i++) { // filling 0th row by 1
+            dp[0][i] = 1;
         }
-        dp[1][0] = 1; 
+        dp[1][0] = 1;
 
-        for(int i = 1;i < m;i++){ // loop m-1 tak hi chelga 
-
-            for(int j = 1;j < n;j++){ // filling 1st row of the dp arr
-                dp[1][j] = dp[0][j] + dp[1][j-1];
-            }
-            for(int j = 1;j < n;j++){ //coping elements of 1st row to 0th row
-                dp[0][j] = dp[1][j] 
+        for (int i = 1; i < m; i++) { // loop m-1 tak hi chelga
+            if (i % 2 == 1) {
+                for (int j = 1; j < n; j++) { // filling 1st row of the dp arr
+                    dp[1][j] = dp[0][j] + dp[1][j - 1];
+                }
+            } else {
+                for (int j = 1; j < n; j++) { // coping elements of 1st row to 0th row
+                    dp[0][j] = dp[1][j] + dp[0][j - 1];
+                }
             }
         }
-        return dp[m-1][n-1];
+        return Math.max(dp[0][n - 1], dp[1][n - 1]);
     }
 }
+
+// optimal approch where space is O(2 * n) = O(n)
+// public int uniquePaths(int m, int n) {
+// int[][] dp = new int[2][n];
+// for(int i = 0;i < n;i++){ // filling 0th row by 1
+// dp[0][i] = 1;
+// }
+// dp[1][0] = 1;
+
+// for(int i = 1;i < m;i++){ // loop m-1 tak hi chelga
+
+// for(int j = 1;j < n;j++){ // filling 1st row of the dp arr
+// dp[1][j] = dp[0][j] + dp[1][j-1];
+// }
+// for(int j = 1;j < n;j++){ //coping elements of 1st row to 0th row
+// dp[0][j] = dp[1][j]
+// }
+// }
+// return dp[m-1][n-1];
+// }
 
 // tabulation approch with TC = O(n*m) and SC = O(n*m)
 // static int[][] dp;
