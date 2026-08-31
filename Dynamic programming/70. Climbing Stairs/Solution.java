c16a75beb3f38ffce7a79c404af4e45bed77fdb5
gargi-sharma18 <2005gargisharma18@gmail.com>
2026-08-31 19:12:27 +0530

    optimal approch of tab in space constant

--- Dynamic programming/70. Climbing Stairs/Solution.java
+++ Dynamic programming/70. Climbing Stairs/Solution.java
@@ -1,21 +1,33 @@
 class Solution {
-    int[] dp;
-
-    public int climbStairs(int n) {
-        dp = new int[n + 1];
-        if (n <= 2)
+    public int climbStairs(int n) { // most optimal approch
+        if (n <= 1)
             return n;
-        return steps(n);
-    }
+        int[] dp = new int[3];
+        dp[0] = 1;
+        dp[1] = 1;
 
-    public int steps(int n) {
-        if (n <= 2)
-            return n;
-        if (dp[n] != 0)
-            return dp[n];
-        return dp[n] = steps(n - 1) + steps(n - 2);
+        for (int i = 2; i <= n; i++) {
+            dp[2] = dp[0] + dp[1];
+            dp[0] = dp[1];
+            dp[1] = dp[2];
+        }
+        return dp[2];
     }
 }
+
+// memoixation approch
+// int[] dp;
+// public int climbStairs(int n) {
+// dp = new int[n+1];
+// if(n <= 2) return n;
+// return steps(n);
+// }
+// public int steps(int n){
+// if(n <= 2) return n;
+// if(dp[n] != 0) return dp[n];
+// return dp[n] = steps(n - 1) + steps(n - 2);
+// }
+// }
 
 // recursive approch
 // public int climbStairs(int n) {
