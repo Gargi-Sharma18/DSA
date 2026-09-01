2c09f04f98bdfa53aea67a572f6459b933671011
gargi-sharma18 <2005gargisharma18@gmail.com>
2026-09-02 00:33:00 +0530

    tabulation optimization

--- Dynamic programming/213. House Robber II/Solution.java
+++ Dynamic programming/213. House Robber II/Solution.java
@@ -1,28 +1,23 @@
 class Solution {
-    int[] dp1;
-    int[] dp2;
-
-    public int rob(int[] nums) {
-        int n = nums.length;
+    public int maxValue(int[] arr) {
+        int n = arr.length;
         if (n == 1)
-            return nums[0];
-        dp1 = new int[n];
-        dp2 = new int[n];
-        Arrays.fill(dp1, -1);
-        Arrays.fill(dp2, -1);
+            return arr[0];
+        if (n == 2)
+            return Math.max(arr[0], arr[1]);
 
-        int case1 = loot(0, n - 1, nums, dp1);
-        int case2 = loot(1, n, nums, dp2);
-        return Math.max(case1, case2);
+        int[] dp1 = new int[n - 1];
+        int[] dp2 = new int[n - 1];
+        return Math.max(loot(0, n - 1, arr, dp1), loot(1, n, arr, dp2));
     }
 
-    public int loot(int idx, int n, int[] arr, int[] dp) {
-        if (idx >= n)
-            return 0;
-        if (dp[idx] != -1)
-            return dp[idx];
-        int pick = arr[idx] + loot(idx + 2, n, arr, dp);
-        int skip = loot(idx + 1, n, arr, dp);
-        return dp[idx] = Math.max(pick, skip);
+    public static int loot(int idx, int n, int[] arr, int[] dp) {
+        dp[0] = arr[idx];
+        dp[1] = Math.max(arr[idx], arr[idx + 1]);
+
+        for (int i = 2; i < n - idx; i++) {
+            dp[i] = Math.max(arr[i + idx] + dp[i - 2], dp[i - 1]);
+        }
+        return dp[n - 1 - idx];
     }
 }
